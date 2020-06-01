374afe209d04d9a1b03dc3ae7827a241117a3777
John Stowers <john.stowers@gmail.com>
2020-06-01 15:33:20 +0200

    video_server: video stim playlists and stim switching

--- flyvr/video/video_server.py
+++ flyvr/video/video_server.py
@@ -1,7 +1,10 @@
 import time
+import uuid
 import os.path
+import collections
 import multiprocessing
 
+import yaml
 import h5py
 import numpy as np
 
@@ -19,15 +22,56 @@ class _Dottable(dict):
         self.__dict__ = self
 
 
+class VideoStimPlaylist(object):
+
+    def __init__(self, *stims):
+        self._stims = collections.OrderedDict()
+        for s in stims:
+            s.show = False
+            self._stims[s.identifier] = s
+
+    def initialize(self, win):
+        [s.initialize(win) for s in self._stims.values()]
+
+    def update_and_draw(self, *args, **kwargs):
+        for s in self._stims.values():
+            s.update_and_draw(*args, **kwargs)
+
+    def describe(self):
+        return [{id_: s.describe()} for id_, s in self._stims.items()]
+
+    def update_params(self, identifier, **params):
+        self._stims[identifier].update_params(**params)
+
+    def play_item(self, identifier):
+        for sid, s in self._stims.items():
+            s.show = sid == identifier
+
+    def play(self, stim):
+        self._stims[stim.identifier] = stim
+
+
 class VideoStim(object):
     NAME = 'grating'
     NUM_VIDEO_FIELDS = 7
 
     def __init__(self, **params):
+        self._id = params.pop('identifier', uuid.uuid4().hex)
+
         self.p = _Dottable(params)
+        self.show = params.pop('show', True)
+
+    @property
+    def identifier(self):
+        return str(self._id)[:32]
 
     def initialize(self, win):
         raise NotImplementedError
+
+    def update_and_draw(self, *args, **kwargs):
+        if self.show:
+            self.update(*args, **kwargs)
+            self.draw()
 
     def update(self, win, logger, frame_num):
         raise NotImplementedError
@@ -85,11 +129,11 @@ class GratingStim(VideoStim):
     # 5: object 1: color
     # 6: object 1: phase
 
-    def __init__(self, sf=50, stim_size=5, stim_color=-1, bg_color=0.5):
+    def __init__(self, sf=50, stim_size=5, stim_color=-1, bg_color=0.5, **kwargs):
         super().__init__(sf=int(sf),
                          stim_size=int(stim_size),
                          stim_color=int(stim_color),
-                         bg_color=float(bg_color))
+                         bg_color=float(bg_color), **kwargs)
         self.screen = None
 
     def initialize(self, win):
@@ -117,11 +161,11 @@ class MovingSquareStim(VideoStim):
     NUM_VIDEO_FIELDS = 7
 
     def __init__(self, size=(0.25, 0.25), speed=(0.01, 0), offset=(0.2, -0.5),
-                 bg_color=-1, fg_color=1):
+                 bg_color=-1, fg_color=1, **kwargs):
         super().__init__(size=[float(size[0]), float(size[1])],
                          speed=[float(speed[0]), float(speed[1])],
                          offset=[float(offset[0]), float(offset[1])],
-                         bg_color=float(bg_color), fg_color=float(fg_color))
+                         bg_color=float(bg_color), fg_color=float(fg_color), **kwargs)
         self.screen = None
 
     def initialize(self, win):
@@ -154,9 +198,9 @@ class PipStim(VideoStim):
     NAME = 'pipstim'
     NUM_VIDEO_FIELDS = 7
 
-    def __init__(self, filename='pipStim.mat', offset=(0.2, -0.5), bg_color=-1, fg_color=1):
+    def __init__(self, filename='pipStim.mat', offset=(0.2, -0.5), bg_color=-1, fg_color=1, **kwargs):
         super().__init__(offset=[float(offset[0]), float(offset[1])],
-                         bg_color=float(bg_color), fg_color=float(fg_color))
+                         bg_color=float(bg_color), fg_color=float(fg_color), **kwargs)
 
         with h5py.File(filename, 'r') as f:
             self._tdis = f['tDis'][:, 0]
@@ -194,10 +238,10 @@ class LoomingStim(VideoStim):
     NUM_VIDEO_FIELDS = 7
 
     def __init__(self, size_min=0.05, size_max=0.8, speed=0.01, offset=(0.2, -0.5),
-                 bg_color=-1, fg_color=1):
+                 bg_color=-1, fg_color=1, **kwargs):
         super().__init__(size_min=float(size_min), size_max=float(size_max), speed=float(speed),
                          offset=[float(offset[0]), float(offset[1])],
-                         bg_color=float(bg_color), fg_color=float(fg_color))
+                         bg_color=float(bg_color), fg_color=float(fg_color), **kwargs)
         self.screen = None
 
     def initialize(self, win):
@@ -228,9 +272,9 @@ class MayaModel(VideoStim):
     NAME = 'maya_model'
     NUM_VIDEO_FIELDS = 7
 
-    def __init__(self, bg_color=(178 / 256) * 2 - 1, frame_start=10000, offset=(0.2, -0.5)):
+    def __init__(self, bg_color=(178 / 256) * 2 - 1, frame_start=10000, offset=(0.2, -0.5), **kwargs):
         super().__init__(bg_color=float(bg_color),
-                         offset=[float(offset[0]), float(offset[1])])
+                         offset=[float(offset[0]), float(offset[1])], **kwargs)
 
         self._imgs = self.screen = None
         self._image_names = ['mayamodel/femalefly360deg/fly' + str(img_num) + '.png' for img_num in range(-179, 181)]
@@ -279,9 +323,9 @@ class OptModel(VideoStim):
     NAME = 'opt_model'
     NUM_VIDEO_FIELDS = 7
 
-    def __init__(self, bg_color=(178 / 256) * 2 - 1, offset=(0.2, -0.5)):
+    def __init__(self, bg_color=(178 / 256) * 2 - 1, offset=(0.2, -0.5), **kwargs):
         super().__init__(bg_color=float(bg_color),
-                         offset=[float(offset[0]), float(offset[1])])
+                         offset=[float(offset[0]), float(offset[1])], **kwargs)
 
         self._imgs = self.screen = None
 
@@ -395,10 +439,12 @@ class VideoServer(object):
 
     def _play(self, stim):
         print("Playing: %r" % stim)
-        assert isinstance(stim, VideoStim)
-        assert self.mywin
-        stim.initialize(self.mywin)
-        self.stim = stim
+        if isinstance(stim, (VideoStim, VideoStimPlaylist)):
+            assert self.mywin
+            stim.initialize(self.mywin)
+            self.stim = stim
+        elif isinstance(stim, str):
+            self.stim.play_item(stim)
 
     def start_stream(self):
         self.task.start()
@@ -449,14 +495,13 @@ class VideoServer(object):
 
             if msg_receiver.poll():
                 msg = msg_receiver.recv()
-                if isinstance(msg, VideoStim):
+                if isinstance(msg, (VideoStim, VideoStimPlaylist, str)):
                     self._play(msg)
                 else:
                     raise NotImplementedError
 
             if self.stim is not None:
-                self.stim.update(self.mywin, self.logger, frame_num=self.samples_played)
-                self.stim.draw()
+                self.stim.update_and_draw(self.mywin, self.logger, frame_num=self.samples_played)
 
                 if self.sync_signal > 60 * 10:
                     self.synchRect.fillColor = 'black'
@@ -465,7 +510,7 @@ class VideoServer(object):
                     self.synchRect.fillColor = 'white'
 
                 self.synchRect.draw()
-                self.mywin.update()
+                self.mywin.flip()
 
                 self.samples_played += 1
                 self.sync_signal += 1
@@ -530,12 +575,31 @@ class VideoStreamProxy:
             time.sleep(0.1)
 
 
+def _build_playlist(yaml_path):
+    stims = []
+
+    with open(yaml_path, 'rt') as f:
+        dat = yaml.load(f)
+        try:
+            for item_def in dat['playlist']['video']:
+                id_, defn = item_def.popitem()
+                stims.append(stimulus_factory(defn.pop('name'), identifier=id_, **defn))
+        except KeyError:
+            pass
+
+    return VideoStimPlaylist(*stims)
+
+
 def run_video_server(options):
     from flyvr.common import SharedState
     from flyvr.common.logger import DatasetLogServerThreaded
     from flyvr.common.ipc import PlaylistReciever
 
     pr = PlaylistReciever()
+
+    playlist_stim = None
+    if options.stim_playlist:
+        playlist_stim = _build_playlist(options.stim_playlist)
 
     with DatasetLogServerThreaded() as log_server:
         logger = log_server.start_logging_server(options.record_file.replace('.h5', '.video_server.h5'))
@@ -551,6 +615,9 @@ def run_video_server(options):
                                    shared_state=state)
         video_client = video_server.start_stream()
 
+        if playlist_stim is not None:
+            video_client.play(playlist_stim)
+
         print('Waiting For Video')
         time.sleep(10)  # takes a bit for the video_server thread to create the psychopy window
 
@@ -558,11 +625,14 @@ def run_video_server(options):
             elem = pr.get_next_element()
             if elem:
                 try:
-                    defn = elem['video']
-                    stim = stimulus_factory(defn['name'], **defn.get('configuration', {}))
-                    video_client.play(stim)
-                except KeyError:
-                    print("Ignoring Message")
+                    if 'video' in elem:
+                        defn = elem['video']
+                        stim = stimulus_factory(defn['name'], **defn.get('configuration', {}))
+                        video_client.play(stim)
+                    elif 'video_item' in elem:
+                        video_client.play(elem['video_item']['identifier'])
+                    else:
+                        print("Ignoring Message")
                 except Exception as exc:
                     print("-------", exc)
 
