bc9c03aae11c3fdcf28375a15fcc2e39865ad021
John Stowers <john.stowers@gmail.com>
2020-10-09 14:18:49 +0200

    tests: remove old arduino ball control, uses phidgets now

--- tests/ball_control_phidget/ball_control.py
+++ tests/ball_control_phidget/ball_control.py
@@ -4,14 +4,17 @@ import random
 
 from Phidget22.Phidget import *
 from Phidget22.Devices.Stepper import *
-import time
+from Phidget22.Net import Net
 
-vel = int(sys.argv[1])
-assert vel < 3000
-assert vel > -3000
-print("===========", vel)
+from flyvr.hwio.phidget import DEFAULT_REMOTE
 
-def main():
+
+def main(initial_vel, remote_details=None):
+    if remote_details:
+        host, port = remote_details
+        print("NETWORK", remote_details)
+        Net.addServer('localhost', host, port, '', 0)
+
     stepper0 = Stepper()
 
     stepper0.setHubPort(0)
@@ -19,29 +22,34 @@ def main():
     stepper0.openWaitForAttachment(5000)
     stepper0.setControlMode(StepperControlMode.CONTROL_MODE_RUN)
     stepper0.setCurrentLimit(0.8)
-    #stepper0.setMaxVelocityLimit(+3000)
-    #stepper0.setMinVelocityLimit(-3000)
 
-    stepper0.setVelocityLimit(vel)
+    print("Attached: ", stepper0.getAttached())
 
-    #stepper0.setTargetPosition(10000)
+    if initial_vel is not None:
+        vels = (initial_vel,)
+    else:
+        vels = (-460, -230, 0, 230, 460)
+
     stepper0.setEngaged(True)
 
     try:
         while True:
-            v = random.choice((-460, -230, 0, 230, 460))
+            v = random.choice(vels)
             stepper0.setVelocityLimit(v)
             print("==", v)
             time.sleep(5)
     except KeyboardInterrupt:
         pass
-    #try:
-    #    input("Press Enter to Stop\n")
-    #except (Exception, KeyboardInterrupt):
-    #    pass
 
+    print("==", 0)
     stepper0.setVelocityLimit(0)
-
     stepper0.close()
 
-main()
+if __name__ == '__main__':
+    try:
+        vel = int(sys.argv[1])
+        assert vel < 3000
+        assert vel > -3000
+    except IndexError:
+        vel = None
+    main(vel, remote_details=DEFAULT_REMOTE)
